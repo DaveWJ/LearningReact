66b12004e990a095481158d1ad318c5054c2be2d
david <davewj>
2017-02-20 00:04:41 -0800

    Added AlbumList file with boilerplate code

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,15 @@
+/**
+ * Created by david on 2/19/17.
+ */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>Hello World!</Text>
+        </View>
+    );
+};
+
+export default AlbumList;
