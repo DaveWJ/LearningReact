a75aecccd210c565469b8bc6feb325bcfc3cf2f3
david <davewj>
2017-02-20 00:12:43 -0800

    Refactored AlbumList from functional componenet to class component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,15 +1,17 @@
 /**
  * Created by david on 2/19/17.
  */
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Hello World!</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World!</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
