245c85f37552a4252baa426d1a8f69f5784bd89b
david <davewj>
2017-02-22 18:03:13 -0800

    Updated AlbumList to use setState and rerender after http request finishes

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,17 +1,21 @@
 /**
  * Created by david on 2/19/17.
  */
+
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = {albums: []};
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({albums: response.data}));
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Hello World!</Text>
