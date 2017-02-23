f51d5627fdd0fe37f443ce595be5b71752b2b1e9
david <davewj>
2017-02-23 14:29:26 -0800

    Added Card and CardSection for styling

---
+++ albums/src/components/CardSection.js
@@ -0,0 +1,16 @@
+/**
+ * Created by david on 2/22/17.
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CardSection = () => {
+    return (
+        <View>
+            <Text>Hello World!</Text>
+        </View>
+    );
+};
+
+export default CardSection;
