a9ed1842fbd0ba43cb8ba94c5e01e1d83dc43756
david <davewj>
2017-02-24 14:22:44 -0800

    Reformated spacing of code

--- auth/src/components/common/Header.js
+++ auth/src/components/common/Header.js
@@ -8,32 +8,32 @@ import { Text, View } from 'react-native';
 
 // make component
 const Header = (props) => {
-    const {textStyle, viewStyle} = styles;
+  const {textStyle, viewStyle} = styles;
 
-    return (
-        <View style={viewStyle}>
-            <Text style={textStyle}>{props.headerText}</Text>
-        </View>
-    );
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
 };
 
 const styles = {
-    viewStyle: {
-        backgroundColor: '#e2f5f8',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 60,
-        paddingTop: 15,
-        shadowColor: '#000',
-        shadowOffset: {width: 0, height: 2},
-        shadowOpacity: 0.5,
-        elevation: 5,
-        position: 'relative'
-    },
-    textStyle: {
-        fontSize: 20,
+  viewStyle: {
+    backgroundColor: '#e2f5f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 5,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20,
 
-    }
+  }
 };
 
 // make available for index file
