59114c1e9a3ab5004e36b4a36170431063b87a67
david <davewj>
2017-02-24 14:01:34 -0800

    Added old files and centralized to app.js

---
+++ auth/src/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by david on 2/24/17.
+ */
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>An App!</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
