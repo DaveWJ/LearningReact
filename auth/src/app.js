512e75537b991bbe981f7e20994557e6abf15f76
david <davewj>
2017-02-24 15:15:22 -0800

    Got firebase going and added first textInput

--- auth/src/app.js
+++ auth/src/app.js
@@ -3,15 +3,27 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDXLTjwybe5xteTn6rYzPYC0cFXsCL02WY',
+      authDomain: 'auth-1f5fa.firebaseapp.com',
+      databaseURL: 'https://auth-1f5fa.firebaseio.com',
+      storageBucket: 'auth-1f5fa.appspot.com',
+      messagingSenderId: '592067719799'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication"/>
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
